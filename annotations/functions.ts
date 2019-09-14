cbed843c5b3b878e0c890b9eca49b7ee13c4fbbf
anthonygharvey <anthony@anthonygharvey.com>
2019-09-14 15:18:21 -0400

    Added type annotations

---
+++ annotations/functions.ts
@@ -0,0 +1,31 @@
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' tyupe
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When you declare a variable on one line and initalize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < word.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+// boolean OR number
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
